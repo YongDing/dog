7691e57b24d6c15cde9dd7f7ac1a357c4fef3594
Yong Ding <yongding@Yong-Dings-13-MacBook-Pro-6.local>
2017-08-30 18:25:06 +0800

    Changes to be committed: 	modified:   dog/settings.py

--- dog/settings.py
+++ dog/settings.py
@@ -122,4 +122,4 @@ STATICFILES_DIRS = (
             # os.path.join(BASE_DIR,'static').replace('','/'),
             )
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static").replace('', '/')
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
